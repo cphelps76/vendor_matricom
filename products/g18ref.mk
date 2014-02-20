b182a6e5daacc57e350c96cdb64fbe8727c59a11
Brandon McAnsh <brandonm@matricom.net>
2014-02-20 11:36:31 -0500

    g18ref: Add DEVICE_FIRMWARE_REVISION define for versioning
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- products/g18ref.mk
+++ products/g18ref.mk
@@ -1,6 +1,9 @@
 # Inherit AOSP device configuration for g18ref
 $(call inherit-product, device/amlogic/g18ref/full_g18ref.mk)
 
+# Stable Firmware revision for device
+DEVICE_FIRMWARE_REVISION=1.1.6
+
 # Inherit common product files
 #$(call inherit-product, vendor/matricom/configs/common.mk)
 

ba57b28d7d26e1ef42777ed81988d54dafa84489
Brandon McAnsh <brandonm@matricom.net>
2014-02-26 13:40:37 -0500

    matricom: Remove duplicate apk directory, and other fixes
    * Change perms on files for SSD, update squisher and opticharger
    * Update version for mx2 to 1.1.7S
    
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- products/g18ref.mk
+++ products/g18ref.mk
@@ -2,13 +2,18 @@
 $(call inherit-product, device/amlogic/g18ref/full_g18ref.mk)
 
 # Stable Firmware revision for device
-DEVICE_FIRMWARE_REVISION=1.1.6
+DEVICE_FIRMWARE_REVISION=1.1.7S
 
 # Inherit common product files
 #$(call inherit-product, vendor/matricom/configs/common.mk)
 
 PRODUCT_NAME := matricom_g18ref
-PRODUCT_BRAND := MBX
+PRODUCT_BRAND := G-Box
 PRODUCT_DEVICE := g18ref
-PRODUCT_MODEL := MBX reference board (g18ref)
+PRODUCT_MODEL := Matricom G-Box Midnight MX2
 PRODUCT_MANUFACTURER := MBX
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME='G-Box MX2' \
+    PRODUCT_DEVICE=MX2

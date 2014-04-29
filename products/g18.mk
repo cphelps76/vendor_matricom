bf4e48e7310fcdfcba5be0b4df85a43173a7d922
Brandon McAnsh <brandonm@matricom.net>
2014-04-29 10:16:14 -0400

    vendor: Commonize versioning and split it up
    * Split up version into its 4 parts - w.x.y.z
    
    Set current version to first test towards 1.1.9 (1.1.8.1)
    
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- products/g18.mk
+++ products/g18.mk
@@ -6,9 +6,6 @@ endif
 # Inherit AOSP device configuration for g18
 $(call inherit-product, device/amlogic/g18/full_g18.mk)
 
-# Stable Firmware revision for device
-DEVICE_FIRMWARE_REVISION=1.1.9
-
 # Inherit common product files
 $(call inherit-product, vendor/matricom/configs/common.mk)
 

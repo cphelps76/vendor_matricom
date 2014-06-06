7558d6b96ec8148403c0650d3eca96a54c3dde31
Brandon McAnsh <brandonm@matricom.net>
2014-06-06 15:30:58 -0400

    vendor: Consolidate proprietary files into separate repo
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- products/movutv_g18.mk
+++ products/movutv_g18.mk
@@ -1,31 +1,11 @@
 # Inherit AOSP device configuration for g18
 $(call inherit-product, device/amlogic/g18/full_g18.mk)
 
+# Inherit client configuration files
+$(call inherit-product, vendor/clients/movutv_product.mk)
+
 # Inherit common product files
 $(call inherit-product, vendor/matricom/configs/common.mk)
-
-# Vendor wants full firmware
-VENDOR_WANTS_ALL := true
-
-DEVICE_PACKAGE_OVERLAYS += vendor/matricom/overlay/movutv
-
-# Wants them gapps
-VENDOR_WANTS_GAPPS := true
-
-# Wants SU
-VENDOR_WANTS_SU := true
-
-# No updates for you
-TARGET_INCLUDE_UPDATER := false
-
-# No initlogo
-TARGET_WANTS_INITLOGO_DISABLED := true
-
-# Wants stock android sheen animation
-TARGET_WANTS_STOCK_BOOTANIMATION := true
-
-# Custom res pack with movutv boot logo
-TARGET_CUSTOM_RES_PACKAGE := vendor/matricom/prebuilt/vendor/movutv/res_pack
 
 PRODUCT_NAME := movutv_g18
 PRODUCT_BRAND := G-Box
--- products/royal_g18.mk
+++ products/royal_g18.mk
@@ -1,51 +1,11 @@
 # Inherit AOSP device configuration for g18
 $(call inherit-product, device/amlogic/g18/full_g18.mk)
 
+# Inherit client configuration files
+$(call inherit-product, vendor/clients/nanonation_product.mk)
+
 # Inherit some common product files
 $(call inherit-product, vendor/matricom/configs/common.mk)
-
-VENDOR_WANTS_ALL := false
-FULL_FIRMWARE_BUILD := true
-
-# Stock Applications
-PRODUCT_PACKAGES += \
-    Bluetooth \
-    DeskClock \
-    CertInstaller \
-    DrmProvider \
-    FusedLocation \
-    Gallery2 \
-    InputDevices \
-    LatinIME \
-    Music \
-    MusicFX \
-    Provision \
-    QuickSearchBox \
-    Settings \
-    SystemUI \
-    bluetooth-health \
-    hostapd \
-    wpa_supplicant.conf
-    
-# Custom Vendor Products
-PRODUCT_PACKAGES += CelebrityCrewTV
-
-DEVICE_PACKAGE_OVERLAYS += vendor/matricom/overlay/royal
-
-# ADB required to on by default
-PRODUCT_PROPERTY_OVERRIDES += persist.service.adb.enable=1
-
-# No flash apk needed
-VENDOR_WANTS_FLASH := false
-
-# Vendor needs SU
-VENDOR_WANTS_SU := true
-
-# Vendor doesn't want gapps
-VENDOR_WANTS_GAPPS := false
-
-# Vendor doesn't want xbmc
-VENDOR_WANTS_XBMC := false
 
 PRODUCT_NAME := royal_g18
 PRODUCT_BRAND := G-Box

1661c12fc6dd4e3edfa5fab2cda64e773b159aa3
Brandon McAnsh <brandonm@matricom.net>
2014-06-16 17:24:09 -0400

    g18: Remove client opt-in defines and configs
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- products/g18.mk
+++ products/g18.mk
@@ -1,8 +1,3 @@
-# Inherit vendor first, if any
-ifneq ($(VENDOR_OVERLAY),)
--include vendor/matricom/configs/$(VENDOR_OVERLAY).mk
-endif
-
 # Inherit AOSP device configuration for g18
 $(call inherit-product, device/amlogic/g18/full_g18.mk)
 
@@ -12,11 +7,7 @@ $(call inherit-product, vendor/matricom/configs/common.mk)
 PRODUCT_NAME := matricom_g18
 PRODUCT_BRAND := G-Box
 PRODUCT_DEVICE := g18
-ifneq ($(PRODUCT_MODEL_OVERRIDE),)
-PRODUCT_MODEL := $(PRODUCT_MODEL_OVERRIDE)
-else
 PRODUCT_MODEL := Matricom G-Box Midnight MX2
-endif
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME='G-Box MX2' \

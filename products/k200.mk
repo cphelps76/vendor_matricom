a1bd0ea20b2d8189d4fb3f2397bfd3d5f11e2f60
Brandon McAnsh <brandonm@matricom.net>
2014-06-20 14:41:12 -0400

    products: Clean up device makefiles
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- products/k200.mk
+++ products/k200.mk
@@ -1,13 +1,5 @@
-# Inherit vendor first, if any
-ifneq ($(VENDOR_OVERLAY),)
--include vendor/matricom/configs/$(VENDOR_OVERLAY).mk
-endif
-
 # Inherit AOSP device configuration for k200
 $(call inherit-product, device/amlogic/k200/full_k200.mk)
-
-# Stable Firmware revision for device
-DEVICE_FIRMWARE_REVISION=0.0.1
 
 # Inherit common product files
 $(call inherit-product, vendor/matricom/configs/common.mk)
